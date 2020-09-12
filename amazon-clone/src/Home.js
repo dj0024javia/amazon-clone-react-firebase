9f3137d34415da3fc7298f195198c4cf30b6af00
dj0024javia <31767853+dj0024javia@users.noreply.github.com>
2020-09-12 15:31:10 +0530

    home page complete. wip on checkout page

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -11,25 +11,30 @@ function Home() {
                 </div>
 
                 <div className="home__row">
+                    <Product title="OnePlus Bullets Wireless Z(Blue)" rating={4}
+                        image="https://images-eu.ssl-images-amazon.com/images/I/41ZyVq9XciL._AC_SY200_.jpg" price={1999} />
+                    <Product title="Apple IPhone 11" rating={5}
+                        image="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTJgmvWi0Xab60QEMH_naBnP9esWuosBCBX7hagZgMVKwBJ_C84mT3kWdK-Lf5cvxdjLsgiHbz9piE&usqp=CAc" price={70000} />
+                    <Product title="Apple Airpods 2" rating={4}
+                        image="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTZ40ySbWRPctWmPt-RTi72yoyumFyJ5RFD8rn3SbE5S8vyjF1gRspMlZOBZi5PqnVhqkZybr6ynrw&usqp=CAc" price={23000} />
+
+                    {/* Product */}
+                </div>
+
+                <div className="home__row">
+                    {/* Product */}
+                    <Product title="OnePlus Buds" rating={4}
+                        image="https://images-na.ssl-images-amazon.com/images/I/61-COaSmn3L._SL1500_.jpg" price={1999} />
+                    <Product title="OnePlus Nord" rating={4}
+                        image="https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRpSF9s1M84MZ63-x3U64VU1alEQFljTZPtH468x6DilU9xyKRcBT7yjc5OUMV7NLw5kPTE5CDTwg&usqp=CAc" price={1999} />
+                    {/* Product */}
+                    {/* Product */}
+                </div>
+
+                <div className="home__row">
+                    {/* Product */}
                     <Product title="OnePlus Bullets Wireless Z" rating={4}
                         image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ5yTrhm_6eVg8nO6n80oEBOUx29Up68VQ6dg&usqp=CAU" price={1999} />
-                    <Product />
-                    <Product />
-
-                    {/* Product */}
-                </div>
-
-                <div className="home__row">
-                    {/* Product */}
-                    <Product />
-                    <Product />
-                    {/* Product */}
-                    {/* Product */}
-                </div>
-
-                <div className="home__row">
-                    {/* Product */}
-                    <Product />
                 </div>
             </div>
 
