ba24f14e9f0a7d64d0636d0da6b61a40fbc798cb
dj0024javia <31767853+dj0024javia@users.noreply.github.com>
2020-09-13 23:24:15 +0530

    subtotal logic done

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
+import { useStateProvider } from './StateProvider'
 function Subtotal({ items }) {
+    const [{ basket }, dispatch] = useStateProvider();
+
     return (
         <div className="subtotal">
             <CurrencyFormat renderText={(value) => (
                 <>
-                    <p>Subtotal(0 Items): <strong>0</strong></p>
+                    <p>Subtotal({basket.length} Items): <strong>{value}</strong></p>
                     <small className="subtotal__gift">
                         <input type="checkbox" />This Order Contains a Gift
                     </small>
                 </>
             )}
                 decimalScale={2}
-                value={0}
+                value={
+                    basket?.reduce((total, eachDict) => {
+                        return total + eachDict?.price
+                    }, 0)
+                }
                 displayType={"text"}
                 thousandSeparator={true}
                 prefix={"₹"}
